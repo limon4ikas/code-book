f43236d68dfbc845f164f1b6a97bbb5d02247c44
limon4ikas <limon4ikas@gmail.com>
2021-02-03 02:18:48 +0300

    text from element

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,26 @@
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
 const App = () => {
-  return <h1>hi</h1>;
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const handleClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
