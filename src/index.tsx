791465638737796c880b1265bec0e0ce0fe3cf9c
limon4ikas <limon4ikas@gmail.com>
2021-02-03 06:51:38 +0300

    getting esbuild from unpkg.com

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
 
@@ -33,7 +33,6 @@ const App = () => {
         global: 'window',
       },
     });
-    // console.log(result);
     setCode(result.outputFiles[0].text);
   };
 
