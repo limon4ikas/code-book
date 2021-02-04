8e4d760169dcd711f1edf5d923962da3087a4aa2
limon4ikas <limon4ikas@gmail.com>
2021-02-04 03:49:36 +0300

    App component refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,65 +1,18 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import bundle from './bundler';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
   const [input, setInput] = useState<string>('');
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
+  const [code, setCode] = useState('');
 
   const handleClick = async () => {
-    if (!ref.current) return;
-
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-    // Passing code to iframe
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    const output = await bundle(input);
+    setCode(output);
   };
-
-  const html = `
-  <html>
-    <head></head>
-    <body>
-      <div id="root"></div>
-      <script>
-        window.addEventListener('message', (event) => {
-          try {
-          eval(event.data)
-          } catch (error) {
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>';
-            console.error(error);
-          }
-        }, false)
-      </script>
-    </body>
-  </html>
-  `;
 
   return (
     <div>
@@ -67,19 +20,10 @@ const App = () => {
         initialValue="const a = 1;"
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={(event) => setInput(event.target.value)}
-      ></textarea>
       <div>
         <button onClick={handleClick}>Submit</button>
       </div>
-      <iframe
-        title="preview"
-        ref={iframe}
-        sandbox="allow-scripts"
-        srcDoc={html}
-      />
+      <Preview code={code} />
     </div>
   );
 };
