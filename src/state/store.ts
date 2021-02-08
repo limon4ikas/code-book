4f1bc9daa3247f0491aa498e5aef13a50b2d9457
limon4ikas <limon4ikas@gmail.com>
2021-02-08 06:18:20 +0300

    persisted state

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,4 +2,16 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+const persistedState = localStorage.getItem('reduxState')
+  ? JSON.parse(localStorage.getItem('reduxState') || '{}')
+  : {};
+
+export const store = createStore(
+  reducers,
+  persistedState,
+  applyMiddleware(thunk)
+);
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+});
