5bdf749d8646f92ceac130304ee9303563fb7d8b
limon4ikas <limon4ikas@gmail.com>
2021-02-08 04:31:06 +0300

    progress bar & remove flickering

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,3 +1,4 @@
+import './code-cell.css';
 import { useEffect } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CodeEditor from './code-editor';
@@ -45,15 +46,17 @@ const CodeCell: React.FunctionComponent<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {!bundle || bundle.loading ? (
-          <div className="progress-cover">
-            <progress className="progress is-small is-primary" max="100">
-              Loading...
-            </progress>
-          </div>
-        ) : (
-          <Preview code={bundle.code} error={bundle.error} />
-        )}
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading...
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} error={bundle.error} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
