036c8730c8c5351213a029f542c8e1d48cb6a73f
limon4ikas <limon4ikas@gmail.com>
2021-02-08 11:52:20 +0300

    redux debugging

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,15 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 import _ from 'lodash';
 import { loadState, saveState } from './local-storage';
+
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const persistedState = loadState();
 
 export const store = createStore(
   reducers,
   persistedState,
-  applyMiddleware(thunk)
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 store.subscribe(
