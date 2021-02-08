0253da66b568a7fc0723f67621e216c314c8e67d
limon4ikas <limon4ikas@gmail.com>
2021-02-08 05:16:15 +0300

    showing react components

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -21,11 +21,21 @@ const CodeCell: React.FunctionComponent<CodeCellProps> = ({ cell }) => {
 
     const cumulativeCode: string[] = [
       `
+      import _React from 'react';
+      import _ReactDOM from 'react-dom';
+      
         const show = (value) => {
+          const root = document.querySelector('#root')
+
           if (typeof value === 'object') {
-            document.querySelector('#root').innerHTML = JSON.stringify(value);
+            if (value.$$typeof && value.props) {
+              _ReactDOM.render(value, root)
+            } else {
+              root.innerHTML = JSON.stringify(value);
+            }
+            
           } else {
-            document.querySelector('#root').innerHTML = value;
+            root.innerHTML = value;
           }
         
         };
