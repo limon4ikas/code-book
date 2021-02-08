a6769f43c7b86262c5c5274114aa3d3ace943602
limon4ikas <limon4ikas@gmail.com>
2021-02-08 05:25:21 +0300

    calling show function only in cell we are in

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -19,31 +19,37 @@ const CodeCell: React.FunctionComponent<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
 
-    const cumulativeCode: string[] = [
-      `
-      import _React from 'react';
-      import _ReactDOM from 'react-dom';
-      
-        const show = (value) => {
-          const root = document.querySelector('#root')
+    const showFunction = `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
+    
+      var show = (value) => {
+        const root = document.querySelector('#root')
 
-          if (typeof value === 'object') {
-            if (value.$$typeof && value.props) {
-              _ReactDOM.render(value, root)
-            } else {
-              root.innerHTML = JSON.stringify(value);
-            }
-            
+        if (typeof value === 'object') {
+          if (value.$$typeof && value.props) {
+            _ReactDOM.render(value, root)
           } else {
-            root.innerHTML = value;
+            root.innerHTML = JSON.stringify(value);
           }
-        
-        };
-      `,
-    ];
+          
+        } else {
+          root.innerHTML = value;
+        }
+      
+      };
+    `;
+    const showFunctionNoOp = 'var show = () => {}';
+
+    const cumulativeCode: string[] = [];
 
     for (let c of orderedCells) {
       if (c.type === 'code') {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunction);
+        } else {
+          cumulativeCode.push(showFunctionNoOp);
+        }
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
